b1e45266b0968b7babcb999c9f4cb2306ade41a3
NarnahEG <esthergbadamosi1@gmail.com>
2024-01-08 16:30:35 +0100

    Appointment test script

---
+++ cypress/e2e/cura.cy.js
@@ -0,0 +1,30 @@
+/// <reference types="cypress" />
+
+describe('Cura UI Test', () => {
+  it('Appointment Booking Test', () => {
+    //access url
+    cy.visit('https://katalon-demo-cura.herokuapp.com')
+
+    //user login
+    cy.get('body')
+    cy.get('#btn-make-appointment').click();
+    cy.get('.section')
+    cy.get('#txt-username').type('John Doe');
+    cy.get('#txt-password').type('ThisIsNotAPassword');
+
+    //click on the login button
+    cy.get('#btn-login').click();
+
+    //make appointment
+    cy.get('#btn-make-appointment')
+    cy.get('#combo_facility').select('Hongkong CURA Healthcare Center');
+    cy.get('.checkbox-inline').click();
+    cy.get('#radio_program_medicaid').click()
+    cy.get('#txt_visit_date').type('20/01/2024')
+    cy.get('#txt_comment').click({force: true}).type('Patient is asthmatic');
+    cy.get('#btn-book-appointment').click({force: true});
+
+    //verify appointment details page
+    cy.get('#summary').contains('Please be informed that your appointment has been booked as following:');
+  })
+})
