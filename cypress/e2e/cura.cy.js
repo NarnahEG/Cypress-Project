d970d9e9fe32d12f9cbacf4122db487a0bd2e099
NarnahEG <esthergbadamosi1@gmail.com>
2024-01-09 10:03:39 +0100

    Selector included

--- cypress/e2e/cura.cy.js
+++ cypress/e2e/cura.cy.js
@@ -1,32 +1,44 @@
 /// <reference types="cypress" />
+import { url,username,password, facility, date, comment} from './selector.cy';
+//const dotenvPlugin = require('cypress-dotenv');
+
+// module.exports = (on, config) => {
+//   config = dotenvPlugin(config);
+
+//   // other plugins, if any...
+
+//   return config;
+// };
 
 describe('Cura UI Test', () => {
   it('Appointment Booking Test', () => {
+    
+    //const password = Cypress.env('password');
     //access url
-    cy.visit('https://katalon-demo-cura.herokuapp.com')
+    cy.visit(url)
 
     //user login
     cy.get('body')
     cy.get('#btn-make-appointment').click();
     cy.get('.section')
-    cy.get('#txt-username').type('John Doe');
-    cy.get('#txt-password').type('ThisIsNotAPassword');
+    cy.get('#txt-username').type(username);
+    cy.get('#txt-password').type(password);
 
     //click on the login button*/
     cy.get('#btn-login').click();
 
     //make appointment
     cy.get('#btn-make-appointment')
-    cy.get('#combo_facility').select('Hongkong CURA Healthcare Center');
+    cy.get('#combo_facility').select(facility);
     cy.get('.checkbox-inline').click();
     cy.get('#radio_program_medicaid').click()
-    cy.get('#txt_visit_date').type('20/01/2024')
-    cy.get('#txt_comment').click({force: true}).type('Patient is asthmatic');
+    cy.get('#txt_visit_date').type(date)
+    cy.get('#txt_comment').click({force: true}).type(comment);
     cy.get('#btn-book-appointment').click({force: true});
 
     //verify appointment details page
     cy.get('#summary').contains('Please be informed that your appointment has been booked as following:');
-    cy.get('#facility').contains('Hongkong CURA Healthcare Center');
+    cy.get('#facility').contains(facility);
     cy.get('#hospital_readmission').contains('Yes');
   
 
